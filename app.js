155dfb50e27e3eaf8a3f5aeee13058eba9bd161b
CCrisV <cristian.c.vargas.cv@gmail.com>
2024-08-26 14:00:36 -0500

    actualizacion

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-function asignarTextoElemento(elemento,texto){
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-}
-
-
 function BtnEncriptar(){
     let TextoEntrada = document.getElementById('textoUsuario').value;
     /* ---alert de prueba ---*/
     let txt =TextoEntrada.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ");
-    alert(txt);
+    
 
-    asignarTextoElemento('main_salida','Primer Juego');
+    if(txt == ""){
+        alert("Ingrese texto porfavor");
+    }else{
+        document.getElementById("SalidaTexto").value = txt;
+        document.getElementById("ImgMuneco").style.visibility = 'hidden';
+        document.getElementById("DivText").style.visibility = 'hidden';
+    }
+
+
 }
