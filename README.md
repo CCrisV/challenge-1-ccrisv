e21a3fe6205fac93683ee357559ddd34ec61ff7d
CCrisV <cristian.c.vargas.cv@gmail.com>
2024-08-26 22:25:02 -0500

    Version Final

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # challenge-1-ccrisv
 <h> Bienvenido a mi primero proyecto </h>
+<p> El proyecto consiste en un encriptador de texto, el cual permite el ingreso de un texto para encriptar y desencriptar un texto encriptado, es posible copiar el texto dentro de la tarjeta de salida. Desarrollado por cristian vargas </p>
--- app.js
+++ app.js
@@ -1,16 +1,81 @@
 function BtnEncriptar(){
     let TextoEntrada = document.getElementById('textoUsuario').value;
-    /* ---alert de prueba ---*/
     let txt =TextoEntrada.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ");
     
 
     if(txt == ""){
         alert("Ingrese texto porfavor");
-    }else{
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else if(TextoEntrada !== txt){
+        alert("El texto ingresado no debe tener acentos ni caracteres especiales");
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else if(TextoEntrada !== TextoEntrada.toLowerCase()){
+        alert("El texto ingresado no debe tener mayusculas");
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else{
+        txt = txt.replace(/e/mg,"enter");
+        txt = txt.replace(/i/mg,"imes");
+        txt = txt.replace(/a/mg,"ai");
+        txt = txt.replace(/o/mg,"ober");
+        txt = txt.replace(/u/mg,"ufat");
         document.getElementById("SalidaTexto").value = txt;
         document.getElementById("ImgMuneco").style.visibility = 'hidden';
         document.getElementById("DivText").style.visibility = 'hidden';
+        document.getElementById("BtnCopiar").style.visibility = 'visible';
     }
+}
 
+function BtnDesencriptar(){
+    let TextoEntrada = document.getElementById('textoUsuario').value;
+    let txt =TextoEntrada.normalize("NFD").replace(/[$\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g, " ");
+    
 
+    if(txt == ""){
+        alert("Ingrese texto porfavor");
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else if(TextoEntrada !== txt){
+        alert("El texto ingresado no debe tener acentos ni caracteres especiales");
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else if(TextoEntrada !== TextoEntrada.toLowerCase()){
+        alert("El texto ingresado no debe tener mayusculas");
+        document.getElementById("SalidaTexto").value = "";
+        document.getElementById("BtnCopiar").style.visibility = 'hidden';
+        document.getElementById("ImgMuneco").style.visibility = 'visible';
+        document.getElementById("DivText").style.visibility = 'visible';
+    }
+    else{
+        txt = txt.replace(/enter/mg,"e");
+        txt = txt.replace(/imes/mg,"i");
+        txt = txt.replace(/ai/mg,"a");
+        txt = txt.replace(/ober/mg,"o");
+        txt = txt.replace(/ufat/mg,"u");
+        document.getElementById("SalidaTexto").value = txt;
+        document.getElementById("ImgMuneco").style.visibility = 'hidden';
+        document.getElementById("DivText").style.visibility = 'hidden';
+        document.getElementById("BtnCopiar").style.visibility = 'visible';
+    }
+}
+function BtnCopiar(){
+        document.querySelector(".main_salida").select();
+        document.execCommand('copy');
 }
